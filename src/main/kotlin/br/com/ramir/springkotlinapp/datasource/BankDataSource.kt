93890af224e04c20c8f1b91a154ca3f3ae07e24b
Ramir Junior <junior.ribeiro0@live.com>
2023-11-17 16:38:02 -0300

    Datasource mock and its unit test

---
+++ src/main/kotlin/br/com/ramir/springkotlinapp/datasource/BankDataSource.kt
@@ -0,0 +1,9 @@
+package br.com.ramir.springkotlinapp.datasource
+
+import br.com.ramir.springkotlinapp.model.Bank
+
+interface BankDataSource {
+
+    fun retrieveBanks(): Collection<Bank>
+
+}
---
+++ src/main/kotlin/br/com/ramir/springkotlinapp/datasource/mock/MockBankDataSource.kt
@@ -0,0 +1,16 @@
+package br.com.ramir.springkotlinapp.datasource.mock
+
+import br.com.ramir.springkotlinapp.datasource.BankDataSource
+import br.com.ramir.springkotlinapp.model.Bank
+import org.springframework.stereotype.Repository
+
+@Repository
+class MockBankDataSource : BankDataSource {
+    val banks = listOf(
+        Bank("1234", 3.14, 0),
+        Bank("1515", 2.8, 1),
+        Bank("2521", 4.5, 1)
+    )
+
+    override fun retrieveBanks(): Collection<Bank> = banks
+}

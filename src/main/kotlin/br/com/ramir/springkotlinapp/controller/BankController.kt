254f9a96d38a6c519db0533f3b29e7d7dee330ec
Ramir Junior <junior.ribeiro0@live.com>
2023-11-17 19:32:34 -0300

    Creating GET single bank request and its test

--- src/main/kotlin/br/com/ramir/springkotlinapp/controller/BankController.kt
+++ src/main/kotlin/br/com/ramir/springkotlinapp/controller/BankController.kt
@@ -2,7 +2,11 @@ package br.com.ramir.springkotlinapp.controller
 
 import br.com.ramir.springkotlinapp.model.Bank
 import br.com.ramir.springkotlinapp.service.BankService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,6 +14,13 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/banks")
 class BankController(private val service: BankService) {
 
+    @ExceptionHandler(NoSuchElementException::class)
+    fun handleNotFound(exception: NoSuchElementException): ResponseEntity<String> =
+        ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.message)
+
     @GetMapping
     fun getBanks(): Collection<Bank> = service.getBanks()
+
+    @GetMapping("{accountNumber}")
+    fun getBank(@PathVariable accountNumber: String): Bank = service.getBank(accountNumber)
 }
--- src/main/kotlin/br/com/ramir/springkotlinapp/datasource/BankDataSource.kt
+++ src/main/kotlin/br/com/ramir/springkotlinapp/datasource/BankDataSource.kt
@@ -5,5 +5,5 @@ import br.com.ramir.springkotlinapp.model.Bank
 interface BankDataSource {
 
     fun retrieveBanks(): Collection<Bank>
-
+    fun retrieveBank(accountNumber: String): Bank
 }
